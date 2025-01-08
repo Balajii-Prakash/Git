30df730121c72984cf2f27323d2664765a1a1e20
Balajii-Prakash <balajiprakash24.2@gmail.com>
2025-01-08 16:41:54 +0530

    initial

---
+++ task1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Task from './Component/Task'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Task></Task>
+    </div>
+  )
+}
+
+export default App
